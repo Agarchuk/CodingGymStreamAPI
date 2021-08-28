b5ee207375f6bb252771eaf89c1e6b79bfe4e31e
Hanna Harchuk <hanna_harchuk@epam.com>
2021-08-28 13:33:16 +0300

    init

---
+++ src/Project.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+public class Project {
+    private List<Mentor> mentors;
+    private String name;
+    private List<Student> students;
+
+    public Project(List<Mentor> mentors, List<Student> students, String name) {
+        this.mentors = mentors;
+        this.name = name;
+        this.students = students;
+    }
+
+    public List<Mentor> getMentors() {
+        return mentors;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "mentors=" + mentors +
+                ", students=" + students +
+                '}';
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,27 @@
+import java.time.LocalDate;
+
 public class Student {
+    private String name;
+    private LocalDate dateOfJoinProject;
+
+    public Student(String name, LocalDate dateOfJoinProject) {
+        this.name = name;
+        this.dateOfJoinProject = dateOfJoinProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfJoinProject() {
+        return dateOfJoinProject;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", dateOfJoinProject=" + dateOfJoinProject +
+                '}';
+    }
 }
